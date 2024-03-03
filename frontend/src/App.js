577752cd7eb08a4945701e54bb369db724155e6b
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-03-03 15:38:54 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Homepage from "./Pages/Homepage";
+
+
+function App() {
+  return (
+    <div>
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+        hello
+    </div>
+  )
+}
+
+export default Homepage
