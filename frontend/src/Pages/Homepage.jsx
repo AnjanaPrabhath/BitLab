9eb8e3724bdbe3ff34a11e1ea77c3d93b27424cc
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-03-06 23:10:25 +0530

    homepage simply completed

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Compilor from '../Components/Compilor'
 import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
 
 const Homepage = () => {
   return (
     <div>
       <Navbar/>
       <Compilor/>
+      <Footer/>
     </div>
   )
 }
