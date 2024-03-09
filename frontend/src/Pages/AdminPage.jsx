e5a257cb49b1931696c196f820b03750fc36e528
sanda-ni <it21172632@my.sliit.lk>
2024-03-09 13:12:24 +0530

    commit-1

--- frontend/src/Pages/AdminPage.jsx
+++ frontend/src/Pages/AdminPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ProfileNavbar from '../Components/ProfileNavbar'
+import Footer from '../Components/Footer'
+
 
 const AdminPage = () => {
   return (
     <div>
         <ProfileNavbar/>
+        <Footer/>
     </div>
   )
 }
