0fab3bb39990c30859eb701d8f2754ab7dbcbdd0
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-03-17 01:50:23 +0530

    admin page

--- frontend/src/Pages/AdminPage.jsx
+++ frontend/src/Pages/AdminPage.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import ProfileNavbar from '../Components/ProfileNavbar'
-import Footer from '../Components/Footer'
 
 
 const AdminPage = () => {
   return (
     <div>
         <ProfileNavbar/>
-        <Footer/>
+
+        <div className='max-w-[1100px] mx-auto grid md:grid-cols-3 gap-8 mt-[200px] text-white text-xl'>
+
+          
+          
+        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>hello</button>
+        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>hello</button>
+        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>hello</button>
+        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>hello</button>
+        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>hello</button>
+        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>hello</button>
+          
+
+        </div>
+        
     </div>
   )
 }
