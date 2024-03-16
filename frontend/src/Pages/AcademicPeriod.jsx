5d18e712bd79591a774ac651f1c35bad6620d0b5
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-03-17 02:12:58 +0530

    Admin page home done

---
+++ frontend/src/Pages/AcademicPeriod.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AcademicPeriod = () => {
+  return (
+    <div>AcademicPeriod</div>
+  )
+}
+
+export default AcademicPeriod
--- frontend/src/Pages/AdminPage.jsx
+++ frontend/src/Pages/AdminPage.jsx
@@ -7,15 +7,26 @@ const AdminPage = () => {
     <div>
         <ProfileNavbar/>
 
-        <div className='max-w-[1100px] mx-auto grid md:grid-cols-3 gap-8 mt-[200px] text-white text-xl'>
+        <div className='flex max-w-[1100px] justify-between mx-auto mt-[100px]  text-white'>
+          <h2 className='flex flex-col font-medium text-3xl'>
+            Dashboard
+          </h2>
+
+          <h3 className='font-light flex text-xl'>
+            logged in as : Dissanayake D M A P
+          </h3>
+
+        </div>
+
+        <div className='max-w-[1100px] mx-auto grid md:grid-cols-3 gap-8 mt-[100px] text-white text-xl'>
 
           
           
-        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>hello</button>
-        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>hello</button>
-        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>hello</button>
-        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>hello</button>
-        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>hello</button>
+        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>Academic Period</button>
+        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>Student Details</button>
+        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>Learning Portal</button>
+        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>Edit Personal details</button>
+        <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>Session Password</button>
         <button className='bg-[#191919] px-[100px] py-[40px] hover:scale-105 duration-500'>hello</button>
           
 
